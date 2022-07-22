d82da2bcff5e45e6ed62e393341c8ba973bc142a
myAlcid <alexxo3159@gmail.com>
2022-07-22 14:50:49 +0300

    init commit

---
+++ src/main/java/com/test/marketplace_api_intellistart_quiz/MarketplaceApiIntellistartQuizApplication.java
@@ -0,0 +1,13 @@
+package com.test.marketplace_api_intellistart_quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceApiIntellistartQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketplaceApiIntellistartQuizApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/marketplace_api_intellistart_quiz/MarketplaceApiIntellistartQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.marketplace_api_intellistart_quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApiIntellistartQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
